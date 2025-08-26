66ba384e90174d5e42f9e63edc4dcf0f9de307b2
blockvished <198261818+blockvished@users.noreply.github.com>
2025-08-26 11:26:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Intro from "@/components/intro";
+
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <Intro />
+        <p>
+          recent 2 projects and skills
+        </p>
+        <p>
+          recent 2 blogs with page
+        </p>
+        <p>
+          contact with page action
+        </p>
+      </div>
+    </>
+  );
+}
+
+
+
+
---
+++ src/components/intro.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div>Intro with image</div>
+  )
+}
+
+export default Intro
